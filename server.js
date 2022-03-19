bab279d1880eb1a5c899480f62a9456e692386ca
Maria Cardona <mechas8703@hotmail.com>
2022-03-19 13:43:42 -0400

    Controllers, Models and routes

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log('Connected on localhost:${PORT}`)');
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
